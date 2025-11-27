864631fa2d94b955014b6c343220d23572481008
manik-babu <mdmanikbabu48@gmail.com>
2025-11-27 21:48:10 +0600

    Add cart models data management system

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -21,6 +21,18 @@ namespace Shopmate.Models
             this.Quantity = quantity;
             this.product = product;
         }
+    }
+    static class Carts
+    {
+        public static Cart[] carts = new Cart[100];
+        public static int CartCount = 0;
+        public static void Add(string shopOwner, string customer, int quantity, Product product)
+        {
+            carts[CartCount++] = new Cart(shopOwner, customer, quantity, product);
+        }
+        public static void GetByCustomer(string customer)
+        {
 
+        }
     }
 }
--- obj/Debug/net9.0/shopmate-cli.AssemblyInfo.cs
+++ obj/Debug/net9.0/shopmate-cli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopmate-cli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1ab3aeb6e0486f01fc5ce8ce60aafbd3af6d9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405b1b08ee61d07c5d2341979c7b72f148507183")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopmate-cli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopmate-cli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
